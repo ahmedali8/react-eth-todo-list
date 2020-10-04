943df61f937ec369250319fd88d727b2d75553cd
ahmedali8 <imahmedalibhatti@gmail.com>
2020-10-04 11:45:35 +0500

    installed web3 and loaded blockchain at basic level - step 01

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
+
 import './App.css';
 
 function App() {
+
+    const [account, setAccount] = useState('');
+
+    useEffect(() => {
+        loadBlockChainData();
+    }, []);
+
+    const loadBlockChainData = async () => {
+
+        // loading blockchain
+        const web3 = new Web3(Web3.currentProvider || 'HTTP://127.0.0.1:8545');
+        console.log(web3);
+        
+        // getting acoount
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
+    };
+
     return (
         <div>
-            Hello World
+            <h1>Hello World</h1>
+            {account}
         </div>
     );
 }
